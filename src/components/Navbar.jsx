6433c5082e9c22c9d7e8e24de06e19df8945035e
John Rey Tolosa <johnrey.tolosa2@gmail.com>
2025-03-11 00:49:55 +0800

    Component init, tailwind setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Navbar () {
+    return (
+        <div className="bg-blue-500 text-blue p-4 rounded-lg">
+            <p class="text-3xl">
+                Hellow
+            </p>
+        </div>
+    );
+
+};
