a779e89fb3579c48eb1671601888d123ec549ad5
John Rey Tolosa <johnrey.tolosa2@gmail.com>
2025-03-16 01:30:45 +0800

    Updated Home and SPA implementation

---
+++ src/components/Education.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Education () {
+
+    return (
+        <section id="education" className="">
+            <h2 className="text-4xl font-bold text-[#c4efdf] Poppins">Education</h2>
+            <div className="flex flex-wrap -mx-4">
+                <div className="w-1/3 px-4">
+
+                </div>
+            </div>
+        </section>
+    )
+};
---
+++ src/components/Home.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <section
+      id="home"
+      className="relative h-screen flex items-center justify-center"
+    >
+      {/* Text Content */}
+      <div className="text-center absolute inset-0 flex flex-col items-center justify-center z-0 pb-70">
+        {/* <h1 className="text-8xl font-extrabold text-[#c4efdf] font-poppins">
+          Full Stack 
+        </h1>
+        <h1 className="text-9xl font-black text-[#c4efdf] font-poppins">
+          Software Engineer
+        </h1> */}
+        <h1 className="text-5xl sm:text-7xl md:text-6xl lg:text-9xl font-extrabold text-black dark:text-[#c4efdf] text-center">
+          Fullstack <br />
+          <span className="block sm:inline">Software Engineer</span>
+        </h1>
+      </div>
+
+      {/* Image */}
+      <img
+        src="/portrait.png"
+        alt="John Rey's Portrait"
+        className="absolute bottom-0 w-[50%] sm:w-[50%] md:w-[35%] lg:w-[25%] mx-auto left-0 right-0"
+      />
+    </section>
+  );
+}
