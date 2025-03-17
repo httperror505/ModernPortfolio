ea6abd3b4d1067360f3da7127be63ddf52b76b71
John Rey Tolosa <johnrey.tolosa2@gmail.com>
2025-03-17 23:56:33 +0800

    Added animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,25 +6,34 @@ export default function Home() {
       id="home"
       className="relative h-screen flex items-center justify-center"
     >
-      {/* Text Content */}
-      <div className="text-center absolute inset-0 flex flex-col items-center justify-center z-0 pb-70">
-        {/* <h1 className="text-8xl font-extrabold text-[#c4efdf] font-poppins">
+      {/* <h1 className="text-8xl font-extrabold text-[#c4efdf] font-poppins">
           Full Stack 
         </h1>
         <h1 className="text-9xl font-black text-[#c4efdf] font-poppins">
           Software Engineer
         </h1> */}
+
+      {/* <div className="text-center absolute inset-0 flex flex-col items-center justify-center z-0 pb-70">
         <h1 className="text-5xl sm:text-7xl md:text-6xl lg:text-9xl font-extrabold text-black dark:text-[#c4efdf] text-center">
           Fullstack <br />
           <span className="block sm:inline">Software Engineer</span>
         </h1>
-      </div>
+      </div> */}
+
+<div className="text-center absolute inset-0 flex flex-col items-center justify-center z-0 pb-70">
+  <h1 className="text-5xl sm:text-7xl md:text-6xl lg:text-9xl font-extrabold text-black dark:text-[#c4efdf] text-center">
+    <span className="typing-animation-line1">Fullstack</span>
+    <br />
+    <span className="typing-animation-line2">Software&nbsp;&nbsp;&nbsp;Engineer</span>
+  </h1>
+</div>
+
 
       {/* Image */}
       <img
         src="/portrait.png"
         alt="John Rey's Portrait"
-        className="absolute bottom-0 w-[50%] sm:w-[50%] md:w-[35%] lg:w-[25%] mx-auto left-0 right-0"
+        className="absolute bottom-0 w-[50%] sm:w-[50%] md:w-[40%] lg:w-[25%] mx-auto left-0 right-0"
       />
     </section>
   );
