069bb0a636a49d9681abc7df9c8f9ec0947cbb02
John Rey Tolosa <johnrey.tolosa2@gmail.com>
2025-03-19 00:15:35 +0800

    Setting up education

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -3,12 +3,15 @@ import React from "react";
 export default function Education () {
 
     return (
-        <section id="education" className="">
-            <h2 className="text-4xl font-bold text-[#c4efdf] Poppins text-center">Education</h2>
-            <div className="flex flex-wrap -mx-4">
-                <div className="w-1/3 px-4">
-
-                </div>
+        <section id="education" className="flex justify-center p-10">
+            <div className="w-full ">
+                <h2 className="text-5xl font-bold text-[#c4efdf] Poppins text-center">Education</h2>
+            </div>
+            <div className="flex flex-row ">
+                <div className="basis-1/4">1</div>
+                <div className="basis-1/4">1</div>
+                <div className="basis-1/4">1</div>
+                <div className="basis-1/4">1</div>
             </div>
         </section>
     )
