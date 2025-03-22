35be379f758f007a24933b7ee0f6b18fc2a4a736
John Rey Tolosa <johnrey.tolosa2@gmail.com>
2025-03-23 00:13:23 +0800

    Add Footer

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -3,12 +3,12 @@ import React from "react";
 export default function Education () {
 
     return (
-        <section id="education" className="flex justify-center p-10">
+        <section id="education" className="justify-center p-10">
             <div className="w-full ">
                 <h2 className="text-5xl font-bold text-[#c4efdf] Poppins text-center">Education</h2>
             </div>
-            <div className="flex flex-row ">
-                <div className="basis-1/4">1</div>
+            <div className="flex-row ">
+                <div className="basis-1/4 ">1</div>
                 <div className="basis-1/4">1</div>
                 <div className="basis-1/4">1</div>
                 <div className="basis-1/4">1</div>
