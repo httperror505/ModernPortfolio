e921e693035bd1ebe556e5fa17856720af2fbf87
John Rey Tolosa <johnrey.tolosa2@gmail.com>
2025-06-09 01:48:50 +0800

    Restructures home section with animated elements
    Refactors the home section layout for improved responsiveness and visual appeal.
    
    Introduces slide-in animations for image and text elements.
    
    Updates styling for the typing animation to enhance the user experience.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,24 +20,44 @@ export default function Home() {
         </h1>
       </div> */}
 
-<div className="text-center absolute inset-0 flex flex-col items-center justify-center z-0 pb-70">
-  <h1 className="text-5xl sm:text-7xl md:text-6xl lg:text-9xl font-extrabold text-[#0C0C0C] dark:text-[#c4efdf] text-center">
-    <span className="typing-animation-line1">Fullstack</span>
-    <br />
-    <span className="typing-animation-line2">Software&nbsp;&nbsp;&nbsp;Engineer</span>
-  </h1>
-</div>
+      {/* <div className="text-center absolute inset-0 flex flex-col items-center justify-center z-0 pb-70">
+        <h1 className="text-5xl sm:text-7xl md:text-6xl lg:text-9xl font-extrabold text-[#0C0C0C] dark:text-[#c4efdf] text-center">
+          <span className="typing-animation-line1">Fullstack</span>
+          <br />
+          <span className="typing-animation-line2">
+            Software&nbsp;&nbsp;&nbsp;Engineer
+          </span>
+        </h1>
+      </div>
 
-<div>
-  
-</div>
+      <div></div>
 
-      {/* Image */}
       <img
         src="/portrait.png"
         alt="John Rey's Portrait"
         className="absolute bottom-0 w-[50%] sm:w-[50%] md:w-[40%] lg:w-[23%] mx-auto left-0 right-0"
-      />
+      /> */}
+
+      <div className="flex flex-col md:flex-row items-center justify-center min-h-screen p-6 relative overflow-hidden">
+        {/* Image Section */}
+        <div className="w-full md:w-1/3 flex justify-center md:justify-end mb-6 md:mb-0 slide-in-left">
+          <img
+            src="/portrait.png"
+            alt="John Rey's Portrait"
+            className="w-[60%] sm:w-[50%] md:w-[80%] lg:w-[70%] border border-[#c4efdf] rounded-4xl"
+          />
+        </div>
+
+        {/* Text Section */}
+        <div className="w-full md:w-2/3 flex justify-center md:justify-start slide-in-right">
+          <h1 className="text-3xl sm:text-5xl md:text-4xl lg:text-6xl font-extrabold text-[#0C0C0C] dark:text-[#c4efdf] text-center md:text-left">
+            <span className="typing-line typing-line1">Fullstack</span>
+            <span className="typing-line typing-line2 mt-2">
+              Software Engineer
+            </span>
+          </h1>
+        </div>
+      </div>
     </section>
   );
 }
